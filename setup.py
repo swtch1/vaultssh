511100821be3eabc3e2dda00f1dec1de4ed0a783
212563094 <joshua.thornton@ge.com>
2017-10-31 13:55:14 -0400

    Building setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+DESCRIPTION = 'Sign SSH keys with Hashicorp Vault that can then be used to SSH into a server.'
+
+setup(
+    name='vaultssh',
+    version='0.1',
+    description=DESCRIPTION,
+    url='https://github.build.ge.com/CoreTechAutomation/vaultssh',
+    author='Joshua Thornton',
+    author_email='joshua.thornton@ge.com',
+    packages=['vaultssh'],
+    install_requires=[
+        'pycryptodome',
+        'requests',
+        'requests-mock',
+    ],
+    zip_safe=False,
+)
